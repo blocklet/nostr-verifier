f27904fc1b4af807eb84cbf434265d54ccaa2f51
wangshijun <wangshijun2010@gmail.com>
2023-02-02 11:54:16 +0800

    feat: return same response on home

--- api/index.js
+++ api/index.js
@@ -8,62 +8,59 @@ const app = express();
 
 const port = process.env.BLOCKLET_PORT || process.env.PORT || 3030;
 
+const getResponse = () => {
+  const hasPubkey = !!env.preferences.pubkey;
+  if (hasPubkey) {
+    let { pubkey } = env.preferences;
+    const needFormatToHex = pubkey.startsWith(npubPrefix);
+    if (needFormatToHex) {
+      pubkey = converter('npub').toHex(pubkey);
+    }
+
+    // try to remove 0x flag and to lower case
+    pubkey = pubkey.replace('0x', '').toLowerCase();
+
+    return {
+      names: {
+        [env.preferences.username]: pubkey,
+      },
+      relays: {
+        [pubkey]: (env.preferences.relays || []).map((x) => x.relay).filter(Boolean),
+      },
+    };
+  }
+
+  return { names: {}, relays: {} };
+};
+
 app.get('/', (req, res) => {
-  res.json(env.preferences);
+  try {
+    res.json(getResponse());
+  } catch (error) {
+    logger.error('gen response failed: ', error);
+    res.json({ error: 'gen response failed' });
+  }
 });
 
 app.get('/.well-known/nostr.json', (req, res) => {
   const { name = '' } = req.query;
   if (name) {
     if (env.preferences.username !== name) {
-      res.json({
-        names: {
-          [name]: '',
-        },
-        relays: {
-          [name]: [],
-        },
-      });
+      res.json({ names: {}, relays: {} });
       return;
     }
 
-    const hasPubkey = !!env.preferences.pubkey;
-
-    if (hasPubkey) {
-      let { pubkey } = env.preferences;
-      const needFormatToHex = pubkey.startsWith(npubPrefix);
-      if (needFormatToHex) {
-        try {
-          // format npub to hex
-          pubkey = converter('npub').toHex(pubkey);
-        } catch (error) {
-          logger.error('format npub to hex failed: ', error);
-          res.send('format npub to hex failed');
-          return;
-        }
-      }
-
-      // try to remove 0x flag and to lower case
-      pubkey = pubkey.replace('0x', '').toLowerCase();
-
-      res.json({
-        names: {
-          [name]: pubkey,
-        },
-        relays: hasPubkey
-          ? {
-              [pubkey]: (env.preferences.relays || []).map((x) => x.relay).filter(Boolean),
-            }
-          : {},
-      });
-
-      return;
+    try {
+      res.json(getResponse());
+    } catch (error) {
+      logger.error('gen response failed: ', error);
+      res.json({ error: 'gen response failed' });
     }
+  } else {
+    res.json({ names: {}, relays: {} });
   }
-
-  res.json({ names: {}, relays: {} });
 });
 
 app.listen(port, () => {
-  console.log(`Blocklet app listening on port ${port}`);
+  console.log(`ready on port ${port}`);
 });
